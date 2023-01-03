5ade2f471a365e6e933a38398aa58e09474524f1
hishampaloli <88199878+hishampaloli@users.noreply.github.com>
2023-01-03 13:57:21 +0530

    Update minHeap.js

--- DSA/javascript/binaryHeaps/minHeap.js
+++ DSA/javascript/binaryHeaps/minHeap.js
@@ -1,1 +1,61 @@
 
+class MAxbinaryHeap {
+    constructor(){
+        this.values = [];
+    }
+    insert(element){
+        this.values.push(element);
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.values[parentIdx];
+            if(element >= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+    extractMin(){
+        const max = this.values[0];
+        const end = this.values.pop();
+        this.values[0] = end;
+
+        this.sinkDown()
+        return max;
+
+    }
+    sinkDown(){
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while(true){
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild,rightChild;
+            let swap = null;
+
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild < element){
+                    swap = leftChildIdx;
+                }
+            }
+            
+            if( rightChildIdx < length){
+                rightChild = this.values[rightChildIdx];
+                if(swap === null && rightChildIdx < element || swap !==null && rightChild < leftChild){
+                    swap = rightChildIdx;
+                }
+            }
+            
+            if(swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
+}
