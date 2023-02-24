cf7d0cf7dc58360c43dab48ed628c1c674322491
hishampaloli <88199878+hishampaloli@users.noreply.github.com>
2023-02-24 09:32:18 +0530

    Update binaryTree.js

--- DSA/javascript/binarySearchtree/binaryTree.js
+++ DSA/javascript/binarySearchtree/binaryTree.js
@@ -75,6 +75,21 @@ class binarySearchTree {
     if (!found) return undefined;
     return closest;
   }
+  
+     BFS(){
+        let node = this.root;
+        let data = [];
+        let queue = [];
+
+        queue.push(node);
+        while(queue.length){
+            node = queue.shift();
+            data.push(node.value);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        return data;
+    }
 
   DFSinOrder() {
     let data = [];
