cdec12f8b5932842478e42340d9df85632b90cee
hishampaloli <88199878+hishampaloli@users.noreply.github.com>
2023-02-27 17:37:41 +0530

    Update hashTable.js

--- DSA/javascript/HashTable/hashTable.js
+++ DSA/javascript/HashTable/hashTable.js
@@ -1,54 +1,39 @@
-const hash = (key,size)=>{
-    let hashedKey = 0;
+class HashTable {
+  constructor(size=53){
+    this.keyMap = new Array(size);
+  }
 
-    for(let i = 0; i < key.length; i++){
-        hashedKey = key.charCodeAt(i);
+  _hash(key) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0)
+      total = (total * 51 + value) % this.keyMap.length;
     }
-    return hashedKey % size;
-
+    return total;
+  }
+    
+  set(key,value){
+    let index = this._hash(key);
+    if(!this.keyMap[index]){
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+      return "value setted in" + index
+  }
+    
+  get(key){
+    let index = this._hash(key);
+    if(this.keyMap[index]){
+      return this.keyMap[index]
+    }
+    return undefined;
+  }
 }
 
+let ht = new HashTable(17);
 
-class HashTable{
-    constructor(){
-        this.size = 20;
-        this.buckets = Array(this.size);
-
-        for(let i = 0; i < this.buckets.length;i++){
-            this.buckets[i] = new Map();
-        }
-
-    }
-
-    insert(key,value){
-        let idx = hash(key,this.size);
-        this.buckets[idx].set(key,value)
-    };
-
-    remove(key){
-        let idx = hash(key,this.size);
-        let deleted = this.buckets[idx].get(key);
-        this.buckets[idx].delete(key);
-        return deleted;
-    }
-
-    search(key){
-        let idx = hash(key,this.size);
-        return this.buckets[idx].get(key);
-    }
-}
-
-
-const hashTable = new HashTable();
-
-hashTable.insert('anoop','fullstack developer');
-hashTable.insert('isudeen','meanstack developer');
-hashTable.insert('renjith','ml stack developer');
-hashTable.insert('hisham','microservice developer');
-hashTable.insert('dileep','mernstack developer');
-console.log(hashTable);
-hashTable.remove('anoop');
-console.log(hashTable);
-hashTable.search('renjith');
-
-console.log(hashTable);
+ht.set("student","hisham")
+ht.set("hi","HI")
+ht.set("hello","THIS IS HELLO")
+ht.get("hello")
