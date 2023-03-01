75963c18a0ba0e9a3ef57f98d2f8b8bd17b93c61
Hisham Paloli <hishampaloli786@gmail.com>
2023-03-01 11:15:07 +0530

    middle element in a linked list

--- DSA/javascript/linkedListOperations/quetions.js
+++ DSA/javascript/linkedListOperations/quetions.js
@@ -1,28 +1,48 @@
-
 //TODO: get nth node from the end
 
-class Solution {
-    
-  getNthFromLast(head, n) {
-    let index = 0;
-    let countHead = head;
-    while (countHead) {
-      countHead = countHead.next;
-      index++;
-    }
+// class Solution {
 
-    let nodeIndex = index - n + 1;
-    index = 1;
+//   getNthFromLast(head, n) {
+//     let index = 0;
+//     let countHead = head;
+//     while (countHead) {
+//       countHead = countHead.next;
+//       index++;
+//     }
 
-    if (nodeIndex <= 0) return -1;
+//     let nodeIndex = index - n + 1;
+//     index = 1;
 
-    while (head) {
-      if (index === nodeIndex) {
-        return head.data;
-      } else {
-        index++;
-        head = head.next;
-      }
-    }
-  }
-}
+//     if (nodeIndex <= 0) return -1;
+
+//     while (head) {
+//       if (index === nodeIndex) {
+//         return head.data;
+//       } else {
+//         index++;
+//         head = head.next;
+//       }
+//     }
+//   }
+// }
+
+// TODO: find the middle node in linked list
+
+// class Solution {
+//   getMiddle(node) {
+//     let index = 0;
+//     let head = node;
+
+//     while (head) {
+//       index++;
+//       head = head.next;
+//     }
+//     let middleIndex = index % 2 == 0 ? index / 2 + 1 : (index - 1) / 2 + 1;
+//     index = 0;
+//     while (node) {
+//       index++;
+//       if (index === middleIndex) return node.data;
+//       node = node.next;
+//     }
+//   }
+// }
