9ace400a0b76079c9566389fbe63bbf0abe943f3
Hisham Paloli <hishampaloli786@gmail.com>
2023-03-04 10:49:51 +0530

    remove loop in linked list

--- DSA/javascript/linkedListOperations/singlylinkedList.js
+++ DSA/javascript/linkedListOperations/singlylinkedList.js
@@ -512,3 +512,46 @@
 //     return dummyList.next;
 //   }
 // }
+
+
+// TODO: remove loop in a linked list
+// class Solution {
+//   //Function to remove a loop in the linked list.
+//   removeLoop(head) {
+//     let slow = head;
+//     let fast = head;
+//     let loopCount = 0;
+//     let isLoop = false;
+
+//     while (fast && fast.next) {
+//       fast = fast.next.next;
+//       slow = slow.next;
+//       if (isLoop) loopCount++;
+//       if (isLoop && fast == slow) break;
+//       if (fast == slow) isLoop = true;
+//     }
+
+//     let loopStartingNode;
+//     let h1 = head;
+//     let h2 = head;
+
+//     for (let i = 0; i < loopCount; i++) {
+//       h1 = h1.next;
+//     }
+
+//     while (h2) {
+//       if (h2 == h1) {
+//         loopStartingNode = h2;
+//         break;
+//       }
+//       h2 = h2.next;
+//       h1 = h1.next;
+//     }
+
+//     let startingNode = loopStartingNode;
+//     for (let i = 1; i < loopCount; i++) {
+//       startingNode = startingNode.next;
+//     }
+//     if (isLoop) startingNode.next = null;
+//   }
+// }
