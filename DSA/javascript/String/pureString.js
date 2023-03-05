8e4f60f548751c21ca0fc436bdb33283f9f64166
Hisham Paloli <hishampaloli786@gmail.com>
2023-03-05 12:10:28 +0530

    ✅

--- DSA/javascript/String/pureString.js
+++ DSA/javascript/String/pureString.js
@@ -41,3 +41,56 @@
 //     return 1;
 //   }
 // }
+
+// TODO: find the common prefix in array of string;
+
+//Input:   arr = [geeksforgeeks, geeks, geek, geezer]
+// Output: gee
+
+// class Solution {
+//   longestCommonPrefix(arr, n) {
+//     let pr = "";
+
+//     for (let i = 0; i < arr[0].length; i++) {
+//       let check = false;
+//       for (let j = 0; j < arr.length; j++) {
+//         if (arr[0][i] === arr[j][i]) {
+//           check = true;
+//         } else {
+//           check = false;
+//           break;
+//         }
+//       }
+//       if (check) {
+//         pr += arr[0][i];
+//       } else {
+//         break;
+//       }
+//     }
+
+//     return pr ? pr : -1;
+//   }
+// }
+
+// TODO: Find the first non repeating letter in string
+
+// Input:  hello
+// Output: h
+
+// class Solution {
+//   //Function to find the first non-repeating character in a string.
+//   nonrepeatingCharacter(s) {
+//     let letTable = {};
+
+//     for (let i = 0; i < s.length; i++) {
+//       if (letTable[s[i]]) letTable[s[i]] = "rep";
+//       if (!letTable[s[i]]) letTable[s[i]] = "uni";
+//     }
+
+//     for (let i = 0; i < s.length; i++) {
+//       if (letTable[s[i]] === "uni") return s[i];
+//     }
+
+//     return "$";
+//   }
+// }
