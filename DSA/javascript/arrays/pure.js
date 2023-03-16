382a1ab886131771db8b466e6cf24eb309209c22
Hisham Paloli <hishampaloli786@gmail.com>
2023-03-16 10:24:15 +0530

    .

--- DSA/javascript/arrays/pure.js
+++ DSA/javascript/arrays/pure.js
@@ -364,3 +364,14 @@
 
 //     }
 // }
+
+// TODO: array search
+
+class Solution {
+    search(arr, N, X) {
+        for(let i = 0; i < N; i++){
+            if(arr[i] == X) return i
+        }
+        return -1
+    }
+}
