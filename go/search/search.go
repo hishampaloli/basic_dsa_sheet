8b8027839536d594f90d0144eb0f4fa3be254a8b
hishampaloli <hishampaloli786@gmail.com>
2023-12-16 18:47:30 +0530

    binary search

--- go/search/search.go
+++ go/search/search.go
@@ -1,3 +1,7 @@
+package main
+
+import "fmt"
+
 func linearSearch(arr []int, val int) bool {
 	for i := 0; i < len(arr); i++ {
 		if arr[i] == val {
@@ -5,4 +9,37 @@ func linearSearch(arr []int, val int) bool {
 		}
 	}
 	return false
+}
+
+func binarySearch(arr []int, val int) int {
+	start, end := 0, len(arr)-1
+	for start <= end {
+		middle := (start + end) / 2
+		if arr[middle] == val {
+			return middle
+		} else if arr[middle] < val {
+			start = middle + 1
+		} else {
+			end = middle - 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	arr := []int{1, 2, 4, 5, 8, 10, 12, 15}
+	valToSearch := 8
+
+	if linearSearch(arr, valToSearch) {
+		fmt.Printf("%d found using Linear Search\n", valToSearch)
+	} else {
+		fmt.Printf("%d not found using Linear Search\n", valToSearch)
+	}
+
+	index := binarySearch(arr, valToSearch)
+	if index != -1 {
+		fmt.Printf("%d found at index %d using Binary Search\n", valToSearch, index)
+	} else {
+		fmt.Printf("%d not found using Binary Search\n", valToSearch)
+	}
 }
