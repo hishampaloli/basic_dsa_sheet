07dafae9c03d03870e1da56930eae97327604e17
hishampaloli <hishampaloli786@gmail.com>
2023-12-20 15:55:26 +0530

    isPalindrome

--- go/string/string.go
+++ go/string/string.go
@@ -13,8 +13,23 @@ func reverseWords(s string) string {
     return strings.Join(words, ".")
 }
 
+
+
+func isPalindrome(s string) int {
+    for i, j := 0, len(s)-1; i < len(s)/2; i, j = i+1, j-1 {
+        if s[i] != s[j] {
+            return 0
+        }
+    }
+    return 1
+}
+
+
 func main() {
     sentence := "i.like.this.program.very.much"
     reversed := reverseWords(sentence)
     fmt.Println(reversed)
+
+	result := isPalindrome("malayalam")
+    fmt.Println(result)
 }
