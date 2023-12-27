bde2d8fb420f6427bba5c7a9fbf9d9293eab7a74
hishampaloli <hishampaloli786@gmail.com>
2023-12-27 09:33:57 +0530

    stack

---
+++ go/stack/stack.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+type Node struct {
+    value string
+    next  *Node
+}
+
+type Stack struct {
+    first *Node
+    last  *Node
+    size  int
+}
+
+func (s *Stack) unshift(val string) int {
+    newNode := &Node{value: val}
+    if s.first == nil {
+        s.first = newNode
+        s.last = newNode
+    } else {
+        temp := s.first
+        s.first = newNode
+        s.first.next = temp
+    }
+    s.size++
+    return s.size
+}
+
+func (s *Stack) shift() string {
+    if s.first == nil {
+        return ""
+    }
+    if s.size == 1 {
+        s.first = nil
+        s.last = nil
+    }
+    temp := s.first
+    s.first = temp.next
+    s.size--
+    return temp.value
+}
+
+func main() {
+    var list Stack
+
+    fmt.Println(list.unshift("786"))
+    fmt.Println(list.unshift("ready"))
+    fmt.Printf("%+v\n", list)
+    fmt.Println(list.shift())
+    fmt.Printf("%+v\n", list)
+}
